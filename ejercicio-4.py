5e3efda017d61df92a523da5d61d520ae034d647
Brayan Vera <t1013600521@unitru.edu.pe>
2022-07-19 22:23:13 -0500

    cambios

--- ejercicio-4.py
+++ ejercicio-4.py
@@ -1,14 +1,14 @@
 def tribonacci(n):
 
-        if n==0:
+        if n==1:
            return 0
-        if n==1 or n==2:
+        if n==2 or n==3:
             return 1
         else:
             return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3)
        
 
-print(tribonacci(int(input('indique el tn a encontrar'))))
+print(tribonacci(int(input('indique el tn a encontrar: '))))
 # print('indique el Tn a encontrar')
 # x=input(int())
 # print(tribonacci(x))
